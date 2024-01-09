9d29f7d52241b37e8d91d17d3afb1ab8a10cf5ff
tathagat.chaurasiya@harness.io <tathagat.chaurasiya@harness.io>
2024-01-09 09:50:13 +0000

    [feat]: [CDS-71873] : remove hack for serviceStepV3Params recaster (#178)

--- 979-recaster/src/main/java/io/harness/utils/RecastReflectionUtils.java
+++ 979-recaster/src/main/java/io/harness/utils/RecastReflectionUtils.java
@@ -131,14 +131,6 @@ public class RecastReflectionUtils {
     Class<?> c = null;
     if (recasterMap.containsIdentifier()) {
       String identifier = (String) recasterMap.getIdentifier();
-
-      // This is hack introduced due to regression reported in CDS-71866. This will be introduced in 795xx
-      // IMPORTANT: To be removed in later releases when no pipeline executions are on waiting before 1686260520000
-      // To track ticket: https://harness.atlassian.net/browse/CDS-71873
-      if ("io.harness.cdng.service.steps.ServiceStepV3Parameters".equals(identifier)) {
-        identifier = "io.harness.cdng.service.steps.helpers.beans.ServiceStepV3Parameters";
-      }
-
       final String documentIdentifier = identifier;
 
       // lets check alias registry first

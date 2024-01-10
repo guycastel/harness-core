b4c914806cedc221306fa3bdc63d6efd645da00a
abhinav.singh@harness.io <abhinav.singh@harness.io>
2024-01-10 09:46:56 +0000

    feat: [CDS-86977]: fix checkstyle (#374)

--- 136-git-sync-manager/src/main/java/io/harness/gitsync/common/scmerrorhandling/handlers/harnesscode/HarnessCodeGenericApiErrorHandler.java
+++ 136-git-sync-manager/src/main/java/io/harness/gitsync/common/scmerrorhandling/handlers/harnesscode/HarnessCodeGenericApiErrorHandler.java
@@ -20,17 +20,19 @@ public class HarnessCodeGenericApiErrorHandler implements ScmApiErrorHandler {
   @Override
   public void handleError(int statusCode, String errorMessage, ErrorMetadata errorMetadata) throws WingsException {
     switch (statusCode) {
-            case 400, 404 ->
-                    throw new ScmBadRequestException(ErrorMessageFormatter.formatMessage(errorMessage, errorMetadata));
-            // throwing not found on 500 status code as we don't ever expect 500 from harness code and as of today most
-            // of the time it is due to not found
-            case 500 -> throw new ScmBadRequestException(ErrorMessageFormatter.formatMessage("not found",
-                    errorMetadata));
-            default -> {
-                log.error(String.format("New error in harness code ops. Status code: [%d]. message: [%s]",
-                        statusCode, errorMessage), new Exception());
-                throw new ScmUnexpectedException(ErrorMessageFormatter.formatMessage(errorMessage, errorMetadata));
-            }
-        }
+      case 400:
+      case 404:
+        throw new ScmBadRequestException(ErrorMessageFormatter.formatMessage(errorMessage, errorMetadata));
+        // throwing not found on 500 status code as we don't ever expect 500 from harness code and as of today most
+        // of the time it is due to not found
+      case 500:
+        throw new ScmBadRequestException(ErrorMessageFormatter.formatMessage("not found", errorMetadata));
+      default: {
+        log.error(
+            String.format("New error in harness code ops. Status code: [%d]. message: [%s]", statusCode, errorMessage),
+            new Exception());
+        throw new ScmUnexpectedException(ErrorMessageFormatter.formatMessage(errorMessage, errorMetadata));
+      }
     }
+  }
 }
